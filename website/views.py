3d868c00f9e00cc8284d129fc8adb8ff05fd5584
Alvian <alviannurfatah@gmail.com>
2023-08-11 11:18:14 +0700

    Final project update

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .form import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -71,4 +71,29 @@ def delete_record(request, pk):
         return redirect('home')
 
 def add_record(request):
-    pass
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added")
+                return redirect('home')
+        else:
+            return render(request, 'website/add_record.html', {'form': form})
+    else:
+        messages.success(request, 'You must logged in to add record')
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "You have been update the record")
+            return redirect('home')
+        else:
+            return render(request, "website/update_record.html", {'form': form, 'recordID': current_record.id})
+    else:
+        messages.success(request, 'You must logged in to update a record')
+        return redirect('home')
